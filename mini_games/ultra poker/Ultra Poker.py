6c25b0e5c530cecb70c407b38a2fa2ad7b354b04
bochk0 <bojidarevtimov14@gmail.com>
2024-08-01 14:27:16 +0300

    working on ultra poker pt.2

--- mini_games/ultra poker/Ultra Poker.py
+++ mini_games/ultra poker/Ultra Poker.py
@@ -470,3 +470,225 @@ def CalculateHandStrength(hand):
 
         if threeOfAKind:
             return 4, valueOfThreeOfAKind
+
+        if twoPair:
+            return 3, valueOfTwoPair
+
+        if pair:
+            return 2, valueOfPair
+
+    
+    if not royalFlush and not straightFlush and not straight and not flush and not fourOfAKind and not fullHouse and not threeOfAKind and not twoPair and not pair:
+        return 1
+    
+
+def Call(TxPlayerCoins, TxPot, FrScrollableText, button):
+    playerMadeDecision.set(True)
+
+    DeductCoinsFromPlayer(lastBet, TxPlayerCoins)
+    UpdatePot(lastBet, TxPot)
+    UpdateFeedback(lastBet, FrScrollableText, False, False)
+    IncreaseWhoseTurn()
+
+    button["state"] = "disabled"
+
+def Check(FrScrollableText):
+    playerMadeDecision.set(True)
+    BtCheck["state"] = "disabled"
+    UpdateFeedback(0, FrScrollableText, False, False)
+    IncreaseWhoseTurn() 
+
+def CommitRaise(EnRaise, TxPlayerCoins, TxPot, FrScrollableText, FrCommandPanelExtension):
+    global lastBet
+
+    raiseValue = EnRaise.get()
+    if raiseValue == "": 
+        raiseValue = 0
+    raiseValue = int(raiseValue) 
+    print(f"player attempted to raise by: {raiseValue}")
+
+    playerCoins = GetPlayerCoins()
+    if playerCoins >= raiseValue and raiseValue > lastBet:
+        lastBet = raiseValue
+        DeductCoinsFromPlayer(raiseValue, TxPlayerCoins)
+        UpdatePot(raiseValue, TxPot),
+        UpdateFeedback(raiseValue, FrScrollableText, False, False)
+        ClearWindowOrFrame(FrCommandPanelExtension)
+        IncreaseWhoseTurn()
+        playerMadeDecision.set(True)
+
+def ShowRaiseMenu(TxPlayerCoins, TxPot, FrScrollableText, FrCommandPanelExtension):
+
+    BtCloseRaiseMenu = Button(FrCommandPanelExtension, text="CANCEL RAISE", font=("Rockwell", 10),
+                        compound="c", image=pixel, height=50, width=108, borderwidth=0, 
+                        command=lambda:ClearWindowOrFrame(FrCommandPanelExtension))
+    BtCloseRaiseMenu.place(x=5,y=130)
+
+    TxRaiseBy = Label(FrCommandPanelExtension, text="RAISE BET BY", font=("Rockwell", 12), background="dark grey")
+    TxRaiseBy.place(x=5,y=5)
+
+    EnRaise = Entry(FrCommandPanelExtension, width=18)
+    EnRaise.place(x=5,y=30)
+
+    BtCommitRaise = Button(FrCommandPanelExtension, text="COMMIT RAISE", font=("Rockwell", 10),
+                        compound="c", image=pixel, height=50, width=108, borderwidth=0, 
+                        command=lambda:CommitRaise(EnRaise, TxPlayerCoins, TxPot, FrScrollableText, FrCommandPanelExtension))
+    BtCommitRaise.place(x=5,y=60)
+
+
+def ReturnPercentageInteger():
+    integer = random.randint(1,100)
+    return integer
+
+def CalculateNextBotBet():
+
+    previousBotChecked = False
+    if previousRoundLastBet == lastBet: 
+        previousBotChecked = True
+
+    if len(allBotHands[whoseTurn - 1]) == 0:
+        return -2 
+
+    botHandStrength = botHandsStrengths[whoseTurn - 1] 
+    if type(botHandStrength) == tuple: 
+        
+        
+        botHandStrength = botHandStrength[0] 
+
+    if botHandStrength == 1: 
+        chance = ReturnPercentageInteger()
+        if chance >= 25: 
+            print(f"bot {whoseTurn} with strength {botHandStrength} CALLED with chance {chance}") 
+            return 1.0 
+        else: 
+            if previousBotChecked: 
+                print(f"bot {whoseTurn} with strength {botHandStrength} CHECKED with chance {chance}") 
+                return 0 
+            else:
+                print(f"bot {whoseTurn} with strength {botHandStrength} FOLDED with chance {chance}") 
+                return -1 
+        
+    elif botHandStrength == 2:
+        chance = ReturnPercentageInteger()
+        if chance >= 30: 
+            print(f"bot {whoseTurn} with strength {botHandStrength} CALLED with chance {chance}") 
+            return 1.0 
+        else: 
+            print(f"bot {whoseTurn} with strength {botHandStrength} RAISED by 1.2x with chance {chance}") 
+            return 1.2 
+
+    elif botHandStrength == 3:
+        chance = ReturnPercentageInteger()
+        if chance >= 35: 
+            print(f"bot {whoseTurn} with strength {botHandStrength} RAISED by 1.5x with chance {chance}") 
+            return 1.5
+        else: 
+            print(f"bot {whoseTurn}with strength {botHandStrength} RAISED by 2.0x with chance {chance}") 
+            return 2.0 
+
+    elif botHandStrength == 4:
+        chance = ReturnPercentageInteger()
+        if chance >= 30: 
+            print(f"bot {whoseTurn} with strength {botHandStrength} RAISED by 2.5x with chance {chance}") 
+            return 2.5
+        else: 
+            print(f"bot {whoseTurn} with strength {botHandStrength} RAISED by 2.0x with chance {chance}") 
+            return 2.0 
+    
+    elif botHandStrength == 5:
+        chance = ReturnPercentageInteger()
+        if chance >= 25: 
+            print(f"bot {whoseTurn} with strength {botHandStrength} RAISED by 3.0x with chance {chance}") 
+            return 3.0 
+        else: 
+            print(f"bot {whoseTurn} with strength {botHandStrength} RAISED by 2.5x with chance {chance}") 
+            return 2.5 
+    
+    elif botHandStrength == 6:
+        chance = ReturnPercentageInteger()
+        if chance >= 20: 
+            print(f"bot {whoseTurn} with strength {botHandStrength} RAISED by 3.5x with chance {chance}") 
+            return 3.5 
+        else: 
+            print(f"bot {whoseTurn} with strength {botHandStrength} RAISED by 3.0x with chance {chance}") 
+            return 3.0 
+
+    elif botHandStrength == 7:
+        chance = ReturnPercentageInteger()
+        if chance >= 15: 
+            print(f"bot {whoseTurn} with strength {botHandStrength} RAISED by 4.0x with chance {chance}") 
+            return 4.0
+        else: 
+            print(f"bot {whoseTurn} with strength {botHandStrength} RAISED by 3.5x with chance {chance}") 
+            return 3.5 
+    
+    elif botHandStrength == 8:
+        chance = ReturnPercentageInteger()
+        if chance >= 10: 
+            print(f"bot {whoseTurn} with strength {botHandStrength} RAISED by 4.5x with chance {chance}") 
+            return 4.5
+        else: 
+            print(f"bot {whoseTurn} with strength {botHandStrength} RAISED by 4.0x with chance {chance}") 
+            return 4.0 
+    
+    elif botHandStrength == 9:
+        chance = ReturnPercentageInteger()
+        if chance >= 5: 
+            print(f"bot {whoseTurn} with strength {botHandStrength} RAISED by 5.0x with chance {chance}") 
+            return 5.0
+        else: 
+            print(f"bot {whoseTurn} with strength {botHandStrength} RAISED by 4.0x with chance {chance}") 
+            return 4.0 
+
+def MakeBotBet(TxPot, FrScrollableText):
+    global lastBet
+
+    if currentRound == 1:
+        UpdatePot(10, TxPot)
+        UpdateFeedback(10, FrScrollableText, False, False)
+        IncreaseWhoseTurn() 
+        lastBet = 10
+    else:
+        UpdateBotHandStrengthsList() 
+        nextBetMultiplier = CalculateNextBotBet() 
+        if nextBetMultiplier == 0:
+            print(f"bot: {whoseTurn - 1} checked")
+        if not len(allBotHands[whoseTurn - 1]) == 0: 
+            if nextBetMultiplier == 1.0: 
+                UpdatePot(lastBet, TxPot)
+                UpdateFeedback(lastBet, FrScrollableText, False, False)
+            elif nextBetMultiplier == -1: 
+                allBotHands[whoseTurn - 1].clear() 
+                UpdateFeedback(lastBet, FrScrollableText, True, False)
+            elif nextBetMultiplier == -2:
+                print(f"no bet made by bot {whoseTurn - 1}, nextbetmultiplier = -2") 
+            elif nextBetMultiplier == 0: 
+                UpdateFeedback(0, FrScrollableText, False, False)
+            else: 
+                UpdatePot(round(lastBet * nextBetMultiplier, 0), TxPot)
+                UpdateFeedback(round(lastBet * nextBetMultiplier, 0), FrScrollableText, False, False)
+                lastBet = round(lastBet * nextBetMultiplier, 0)
+        IncreaseWhoseTurn()
+
+
+def GetSuitOfCardOneAndTwo(firstChosenCard, secondChosenCard):
+
+    if "spades" in firstChosenCard:
+        firstChosenCardSuit = "spades"
+    elif "clubs" in firstChosenCard:
+        firstChosenCardSuit = "clubs"
+    elif "hearts" in firstChosenCard:
+        firstChosenCardSuit = "hearts"
+    elif "diamonds" in firstChosenCard:
+        firstChosenCardSuit = "diamonds"
+
+    if "spades" in secondChosenCard:
+        secondChosenCardSuit = "spades"
+    elif "clubs" in secondChosenCard:
+        secondChosenCardSuit = "clubs"
+    elif "hearts" in secondChosenCard:
+        secondChosenCardSuit = "hearts"
+    elif "diamonds" in secondChosenCard:
+        secondChosenCardSuit = "diamonds"
+
+    return firstChosenCardSuit, secondChosenCardSuit
