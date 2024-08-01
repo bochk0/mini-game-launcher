95e878725d00013355695cc7d22d4ce9b6cba2a8
bochk0 <bojidarevtimov14@gmail.com>
2024-08-01 14:22:14 +0300

    working on ultra poker pt.1

--- mini_games/ultra poker/Ultra Poker.py
+++ mini_games/ultra poker/Ultra Poker.py
@@ -230,3 +230,243 @@ def DeductCoinsFromPlayer(numberOfCoins, TxPlayerCoins):
     
     cursor.execute(f"UPDATE tblStats_{accountUsername} SET currentMoney = {playerCoins}")
     con.commit()
+
+
+def UpdatePlayerCardButtonsWithList(cardButtons):
+
+    
+    for card in range(len(cardButtons)):
+        cardButtons[card].image = ""
+    
+
+    
+    for card in range(len(playerHand)): 
+        cardImageFile = Image.open(f"Playing Cards/{playerHand[card]}")
+        resizedCardImageFile = cardImageFile.resize((114, 164), Image.LANCZOS)
+        actualCardImage = ImageTk.PhotoImage(resizedCardImageFile)
+        cardButtons[card].image = actualCardImage
+        cardButtons[card].config(image=actualCardImage)
+    
+def PlacePlayerCardImage(actualCardImage, cardButtons):
+    
+
+    if len(playerHand) == 1: 
+        cardButtons[0].image = actualCardImage 
+        cardButtons[0].config(image=actualCardImage) 
+    elif len(playerHand) == 2: 
+        cardButtons[1].image = actualCardImage
+        cardButtons[1].config(image=actualCardImage) 
+    elif len(playerHand) == 3: 
+        cardButtons[2].image = actualCardImage
+        cardButtons[2].config(image=actualCardImage) 
+    elif len(playerHand) == 4: 
+        cardButtons[3].image = actualCardImage
+        cardButtons[3].config(image=actualCardImage) 
+    elif len(playerHand) == 5: 
+        cardButtons[4].image = actualCardImage
+        cardButtons[4].config(image=actualCardImage) 
+
+def GeneratePlayerCard(cardButtons):
+    global numOfCards
+
+    
+    randomCardNum = random.randint(0, numOfCards)
+    cardImagePNGtext = cardsList[randomCardNum]
+    cardsList.pop(randomCardNum)
+    numOfCards -= 1
+    
+
+    
+    cardImageFile = Image.open(f"Playing Cards/{cardImagePNGtext}")
+    resizedCardImageFile = cardImageFile.resize((114, 164), Image.LANCZOS)
+    actualCardImage = ImageTk.PhotoImage(resizedCardImageFile)
+    
+
+    playerHand.append(cardImagePNGtext) 
+    PlacePlayerCardImage(actualCardImage, cardButtons)
+
+def GenerateCardForAllBots():
+    global numOfCards, allBotHands
+
+    
+    for bot in range(3):
+        if (currentRound == 2) or (currentRound > 2 and len(allBotHands[bot]) > 0): 
+            randomCardNum = random.randint(0, numOfCards)
+            cardImagePNGtext = cardsList[randomCardNum]
+            cardsList.pop(randomCardNum)
+            numOfCards -= 1
+            allBotHands[bot].append(cardImagePNGtext) 
+
+def GenerateCommunityCard(communityCardList):
+    global numOfCards, communityHand
+
+    
+    randomCardNum = random.randint(0, numOfCards)
+    cardImagePNGtext = cardsList[randomCardNum]
+    cardsList.pop(randomCardNum)
+    numOfCards -= 1
+    
+
+    
+    cardImageFile = Image.open(f"Playing Cards/{cardImagePNGtext}")
+    resizedCardImageFile = cardImageFile.resize((55, 83), Image.LANCZOS)
+    actualCardImage = ImageTk.PhotoImage(resizedCardImageFile)
+    
+
+    communityHand.append(cardImagePNGtext) 
+
+    if len(communityHand) == 1:
+        communityCardList[0].image = actualCardImage
+        communityCardList[0].config(image=actualCardImage)
+    elif len(communityHand) == 2:
+        communityCardList[1].image = actualCardImage
+        communityCardList[1].config(image=actualCardImage)
+    elif len(communityHand) == 3:
+        communityCardList[2].image = actualCardImage
+        communityCardList[2].config(image=actualCardImage)
+
+
+
+def CheckForPairOrKinds(whatToCheckFor, botHand, pair, twoPair, threeOfAKind, fourOfAKind):
+
+    botHandValues = MakeHandIntoListOfValues(botHand)
+    print(f"bot hand values {botHandValues}")
+    numberOfPairs = 0
+    valuesOfPairs = []
+
+    for value in range(14, 1, -1): 
+        if botHandValues.count(value) == whatToCheckFor: 
+            if whatToCheckFor == 2: 
+                pair = True 
+                valueOfPair = value 
+                numberOfPairs += 1
+                valuesOfPairs.append(value)
+                if numberOfPairs == 2:
+                    twoPair = True
+                    valueOfTwoPair = max(valuesOfPairs)
+            elif whatToCheckFor == 3:
+                threeOfAKind = True
+                valueOfThreeOfAKind = value
+            elif whatToCheckFor == 4:
+                fourOfAKind = True
+                valueOfFourOfAKind = value
+                
+    
+    if whatToCheckFor == 2 and pair and not twoPair: 
+        return pair, valueOfPair 
+    elif whatToCheckFor == 2 and twoPair: 
+        return twoPair, valueOfTwoPair 
+    elif whatToCheckFor == 3 and threeOfAKind: 
+        return threeOfAKind, valueOfThreeOfAKind 
+    elif whatToCheckFor == 4 and fourOfAKind: 
+        return fourOfAKind, valueOfFourOfAKind 
+    else:
+        return False, 0
+
+def IsStraight(hand): 
+
+    handValues = MakeHandIntoListOfValues(hand) 
+    handValuesSet = list(set(handValues))
+    handValuesSet.sort() 
+    sectionOfHandValues = []
+    limit = 5
+    if len(handValuesSet) >= limit:
+        for x in range(limit - 1):
+            sectionOfHandValues = handValuesSet[-5-x:len(handValuesSet)-x] 
+            for i in range(limit - 1):
+                if sectionOfHandValues[i] != sectionOfHandValues[i+1] - 1: 
+                    return False
+            return True 
+
+def CheckForFlush(hand):
+    botHandString = '\t'.join(hand) 
+
+    numberOfDiamonds = botHandString.count("diamonds")
+    numberOfHearts = botHandString.count("hearts")
+    numberOfSpades = botHandString.count("spades")
+    numberOfClubs = botHandString.count("clubs")
+
+    if numberOfDiamonds >= 5:
+        return True
+    elif numberOfHearts >= 5:
+        return True
+    elif numberOfSpades >= 5:
+        return True
+    elif numberOfClubs >= 5:
+        return True
+    else:
+        return False
+
+def MakeHandIntoListOfValues(hand):
+    handValues = [] 
+
+    for card in range(len(hand)):
+        valueOfCard = hand[card][0:2] 
+        if valueOfCard[1] == "_": 
+            valueOfCard = valueOfCard[0] 
+        handValues.append(int(valueOfCard)) 
+
+    return handValues  
+
+def CalculateHandStrength(hand):
+
+    royalFlush, straightFlush, fourOfAKind, fullHouse, flush, straight, threeOfAKind, twoPair, pair = False, False, False, False, False, False, False, False, False
+
+    botHandString = '\t'.join(hand) 
+
+    
+    if IsStraight(hand):
+        straight = True
+    
+           
+    
+    try:
+        flush = CheckForFlush(hand) 
+    except: 
+        flush = False
+    
+
+    
+    if flush and straight:
+        
+        if "10" in botHandString and "11" in botHandString and "12" in botHandString and "13" in botHandString and "14" in botHandString:
+            return 10
+        
+        return 9
+        
+    if not straightFlush:
+
+        
+        fourOfAKind, valueOfFourOfAKind = CheckForPairOrKinds(4, hand, pair, twoPair, threeOfAKind, fourOfAKind)
+        
+
+        if fourOfAKind:
+            return 8, valueOfFourOfAKind
+
+    if not fourOfAKind:
+
+        
+        pair, valueOfPair = CheckForPairOrKinds(2, hand, pair, twoPair, threeOfAKind, fourOfAKind) 
+        
+
+        
+        twoPair, valueOfTwoPair = CheckForPairOrKinds(2, hand, pair, twoPair, threeOfAKind, fourOfAKind) 
+        
+
+        
+        threeOfAKind, valueOfThreeOfAKind = CheckForPairOrKinds(3, hand, pair, twoPair, threeOfAKind, fourOfAKind)
+        
+
+        
+        if pair and threeOfAKind:
+            return 7
+        
+
+        if flush:
+            return 6
+
+        if straight:
+            return 5
+
+        if threeOfAKind:
+            return 4, valueOfThreeOfAKind
